a1c2a0f36942ae7204919f0bfccad5c55b474cc9
itamar1996 <itay9919@gmail.com>
2024-09-19 15:14:29 +0300

    include sorting

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ ranges.forEach(range => {
     range.addEventListener("change", () => {
         const label = document.querySelector(`#${range.id}-label`);
         if (label) {            
-            label.textContent = (range as HTMLInputElement).value;
+            label.textContent = range.id +": "+(range as HTMLInputElement).value;
         }
     });
 });
@@ -25,6 +25,12 @@ const GetFormObject = (): FormDetails=>{
     const points1: string = (document.querySelector("#points") as HTMLInputElement).value;
     const pre3: string = (document.querySelector("#pre3") as HTMLInputElement).value;
     const pre2: string = (document.querySelector("#pre2") as HTMLInputElement).value;
+    console.log(pre3);
+    console.log("2 "+pre2);
+    console.log(points1);
+    
+    
+    
     return {
         position: position1,
         twoPercent: parseInt( pre2),
@@ -48,7 +54,10 @@ const GetPlayersArr = async():Promise<FormDetails[]|void>=>{
         if (res.ok) {
             const newItem = await res.json();
             console.log(newItem);
-            displayPlayersFromSearch(newItem as Player[])
+            myplayers = newItem as Player[];
+            displayPlayersFromSearch(myplayers)
+
+            // displayPlayersFromSearch(newItem as Player[])
             return newItem as Player[]
 
         } else {
@@ -60,19 +69,23 @@ const GetPlayersArr = async():Promise<FormDetails[]|void>=>{
     
 }
 const displayPlayersFromSearch = (players :Player[]):void=>{
-    const playerstable = document.querySelector(".search-players-table")!
+    console.log("rdrhbz");
+    
+    const playerstable = document.querySelector(".search-players-table-players")!
     playerstable.innerHTML = "";
     for (const player of players) {
         const playerbox = document.createElement("tr")
         playerbox.id = player._id;
         const playerName = document.createElement("td")
-        const playerPosition = document.createElement("td")
+        const playerPosition = document.createElement("td")                
         const playerPoints = document.createElement("td")
         const player3pre = document.createElement("td")
         const player2pre = document.createElement("td")
         const playeraction = document.createElement("td")
         const playerBtn = document.createElement("button")
+        playerPosition.className = "td-pos";
         playerBtn.textContent = `add ${getFirstWord(player.playerName)} to my team`;
+        playerBtn.className = "add-to-my-team"
         playerBtn.addEventListener("click",()=>addPlayerToMyTeam(player));
         playerName.id ="player-th";
         playeraction.id ="Action-th";
@@ -85,8 +98,8 @@ const displayPlayersFromSearch = (players :Player[]):void=>{
         playerbox.appendChild(playerName);
         playerbox.appendChild(playerPosition);
         playerbox.appendChild(playerPoints);
-        playerbox.appendChild(player3pre);
         playerbox.appendChild(player2pre);
+        playerbox.appendChild(player3pre);
         playerbox.appendChild(playeraction);
         playerstable.appendChild(playerbox);
     }
@@ -100,13 +113,13 @@ const addPlayerToMyTeam = (player :Player)=>{
     console.log(playerbox);
 
     playerbox.innerHTML = "";
-    const playerName = document.createElement("p");
+    const playerName = document.createElement("h4");
     const player3pre = document.createElement("p")
     const player2pre = document.createElement("p")
     const playerPoints = document.createElement("p")
-    playerPoints.textContent = player.points.toString();
-    player2pre.textContent = player.twoPercent.toString();
-    player3pre.textContent = player.threePercent.toString();
+    playerPoints.textContent = "points:"+ player.points.toString();
+    player2pre.textContent = "twoPercent:"+ player.twoPercent.toString()+"%";
+    player3pre.textContent = "threePercent:" + player.threePercent.toString() + "%";
     playerName.textContent = player.playerName;
     playerbox?.appendChild(playerName);
     playerbox?.appendChild(player3pre);
@@ -114,9 +127,40 @@ const addPlayerToMyTeam = (player :Player)=>{
     playerbox?.appendChild(playerPoints);
 }
 
-
+const thPlayers = document.querySelectorAll(".th-p");
+thPlayers.forEach(th=>{
+    th.addEventListener("click",()=>{
+        switch (th.textContent) {
+            case "player":
+                displayPlayersFromSearch(sortPlayersArr(myplayers,"playerName"))
+                break;
+                case "points":
+                displayPlayersFromSearch(sortPlayersArr(myplayers,"points"))
+                break
+                case "FG%":
+            displayPlayersFromSearch(sortPlayersArr(myplayers,"twoPercent"))
+                break;
+                case "3P%":
+            displayPlayersFromSearch(sortPlayersArr(myplayers,"threePercent"))
+                break;
+            default:
+                break;
+        }
+    })
+})
 
 document.querySelector(".search-players")?.addEventListener("click",()=>GetPlayersArr())
+const sortPlayersArr = <T>(playersarr: Player[], type: keyof Player): Player[] => {
+    return playersarr.sort((a, b) => {
+        if (a[type] < b[type]) {
+            return -1;
+        }
+        if (a[type] > b[type]) {
+            return 1;
+        }
+        return 0;
+    });
+}
 const getFirstWord = (sentence: string): string => {
     const firstWord = sentence.split(' ')[0];
     return firstWord;
--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ ranges.forEach(range => {
     range.addEventListener("change", () => {
         const label = document.querySelector(`#${range.id}-label`);
         if (label) {
-            label.textContent = range.value;
+            label.textContent = range.id + ": " + range.value;
         }
     });
 });
@@ -24,6 +24,9 @@ const GetFormObject = () => {
     const points1 = document.querySelector("#points").value;
     const pre3 = document.querySelector("#pre3").value;
     const pre2 = document.querySelector("#pre2").value;
+    console.log(pre3);
+    console.log("2 " + pre2);
+    console.log(points1);
     return {
         position: position1,
         twoPercent: parseInt(pre2),
@@ -45,7 +48,9 @@ const GetPlayersArr = async () => {
         if (res.ok) {
             const newItem = await res.json();
             console.log(newItem);
-            displayPlayersFromSearch(newItem);
+            myplayers = newItem;
+            displayPlayersFromSearch(myplayers);
+            // displayPlayersFromSearch(newItem as Player[])
             return newItem;
         }
         else {
@@ -57,7 +62,8 @@ const GetPlayersArr = async () => {
     }
 };
 const displayPlayersFromSearch = (players) => {
-    const playerstable = document.querySelector(".search-players-table");
+    console.log("rdrhbz");
+    const playerstable = document.querySelector(".search-players-table-players");
     playerstable.innerHTML = "";
     for (const player of players) {
         const playerbox = document.createElement("tr");
@@ -69,7 +75,9 @@ const displayPlayersFromSearch = (players) => {
         const player2pre = document.createElement("td");
         const playeraction = document.createElement("td");
         const playerBtn = document.createElement("button");
+        playerPosition.className = "td-pos";
         playerBtn.textContent = `add ${getFirstWord(player.playerName)} to my team`;
+        playerBtn.className = "add-to-my-team";
         playerBtn.addEventListener("click", () => addPlayerToMyTeam(player));
         playerName.id = "player-th";
         playeraction.id = "Action-th";
@@ -82,8 +90,8 @@ const displayPlayersFromSearch = (players) => {
         playerbox.appendChild(playerName);
         playerbox.appendChild(playerPosition);
         playerbox.appendChild(playerPoints);
-        playerbox.appendChild(player3pre);
         playerbox.appendChild(player2pre);
+        playerbox.appendChild(player3pre);
         playerbox.appendChild(playeraction);
         playerstable.appendChild(playerbox);
     }
@@ -94,20 +102,52 @@ const addPlayerToMyTeam = (player) => {
     const playerbox = playerbox1.querySelector(".players-box-det");
     console.log(playerbox);
     playerbox.innerHTML = "";
-    const playerName = document.createElement("p");
+    const playerName = document.createElement("h4");
     const player3pre = document.createElement("p");
     const player2pre = document.createElement("p");
     const playerPoints = document.createElement("p");
-    playerPoints.textContent = player.points.toString();
-    player2pre.textContent = player.twoPercent.toString();
-    player3pre.textContent = player.threePercent.toString();
+    playerPoints.textContent = "points:" + player.points.toString();
+    player2pre.textContent = "twoPercent:" + player.twoPercent.toString() + "%";
+    player3pre.textContent = "threePercent:" + player.threePercent.toString() + "%";
     playerName.textContent = player.playerName;
     playerbox === null || playerbox === void 0 ? void 0 : playerbox.appendChild(playerName);
     playerbox === null || playerbox === void 0 ? void 0 : playerbox.appendChild(player3pre);
     playerbox === null || playerbox === void 0 ? void 0 : playerbox.appendChild(player2pre);
     playerbox === null || playerbox === void 0 ? void 0 : playerbox.appendChild(playerPoints);
 };
+const thPlayers = document.querySelectorAll(".th-p");
+thPlayers.forEach(th => {
+    th.addEventListener("click", () => {
+        switch (th.textContent) {
+            case "player":
+                displayPlayersFromSearch(sortPlayersArr(myplayers, "playerName"));
+                break;
+            case "points":
+                displayPlayersFromSearch(sortPlayersArr(myplayers, "points"));
+                break;
+            case "FG%":
+                displayPlayersFromSearch(sortPlayersArr(myplayers, "twoPercent"));
+                break;
+            case "3P%":
+                displayPlayersFromSearch(sortPlayersArr(myplayers, "threePercent"));
+                break;
+            default:
+                break;
+        }
+    });
+});
 (_a = document.querySelector(".search-players")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => GetPlayersArr());
+const sortPlayersArr = (playersarr, type) => {
+    return playersarr.sort((a, b) => {
+        if (a[type] < b[type]) {
+            return -1;
+        }
+        if (a[type] > b[type]) {
+            return 1;
+        }
+        return 0;
+    });
+};
 const getFirstWord = (sentence) => {
     const firstWord = sentence.split(' ')[0];
     return firstWord;
